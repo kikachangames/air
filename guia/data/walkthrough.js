8383676e6decc6cc6f0039744ca69e5dbbdbebb3
kikachangames <154857620+kikachangames@users.noreply.github.com>
2024-12-19 18:38:40 -0300

    Update walkthrough.js

--- guia/data/walkthrough.js
+++ guia/data/walkthrough.js
@@ -1105,10 +1105,16 @@ var data = {
         {
           "date": "29 de Julho",
           "choices": [
-            "Linha ”Ta”",
+		  {
+            "choice":"Linha ”Ta”",
 		  "comment": "Cena H / Essa cena não estará presente se você estiver usando o patch Para todas as idades"
-            "Continuar lendo o dicionário",
-            "Agir com ciúmes"1
+		  }
+		  {
+            "choices":"Continuar lendo o dicionário",
+			  }
+			  {
+            "choices":"Agir com ciúmes"
+				  }
           ]
         },
         {
